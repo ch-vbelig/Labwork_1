44605c722f7c45f97e39df3c4749081e8e784b76
Belig Chimitov <belig.ch111@gmail.com>
2021-10-11 13:45:09 +0700

    fifth commit
    Added subtraction function

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ function sum(a, b){
     return a + b
 }
 
+function subtract(a, b){
+    return a - b
+}
+
 let a = 5
 let b = 8
 console.log(`${a} + ${b} = ${sum(a,b)}`)
+
+console.log(`${a} - ${b} = ${subtract(a,b)}`)
